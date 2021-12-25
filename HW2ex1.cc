bc1f5eb99282e5e43649ece27aaaca8322fa1e97
ofirrazon <63589940+ofirrazon@users.noreply.github.com>
2021-12-25 23:27:56 +0200

    Add files via upload
    NEW AND UPDATED

--- HW2ex1.cc
+++ HW2ex1.cc
@@ -2,9 +2,11 @@
 #include <signal.h>
 #include <sstream>
 #include <fstream>
+#include <queue>
 #include "hcm.h"
 #include "flat.h"
 #include "hcmvcd.h"
+#include "hcmsigvec.h"
 
 using namespace std;
 
@@ -19,6 +21,7 @@ bool verbose = false;
 int main(int argc, char **argv) {
   string sigsFileName;
   string vecsFileName;
+  string topLevel;
   int argIdx = 1;
   int anyErr = 0;
   unsigned int i;
@@ -32,13 +35,14 @@ int main(int argc, char **argv) {
       argIdx++;
       verbose = true;
     }
-	sigsFileName = string(argv[argIdx++]);
-	vecsFileName = string(argv[argIdx++]);
 	
     if (!strcmp(argv[argIdx], "-s")) {
       argIdx++;
       shortRun = true;
     }
+	topLevel = string(argv[argIdx++]);
+	sigsFileName = string(argv[argIdx++]);
+	vecsFileName = string(argv[argIdx++]);
 
     for (;argIdx < argc; argIdx++) {
       vlgFiles.push_back(argv[argIdx]);
@@ -58,104 +62,111 @@ int main(int argc, char **argv) {
   hcmSigVec parser(sigsFileName, vecsFileName, verbose);
   set<string> sigs;
   parser.getSignals(sigs);
+  
+  //TODO: debug delete later 
   for (set<string>::iterator I= sigs.begin(); I != sigs.end(); I++) {
 	 cout << "SIG: " << (*I) << endl;
   }
 
-  cout << "-I- Reading vectors ... " << endl;
-  while (parser.readVector() == 0) {
+  
+  set<string> globalNodes;
+  globalNodes.insert("VDD");
+  globalNodes.insert("VSS");
+
+  hcmDesign* design = new hcmDesign("design");
+  //string cellName = vlgFiles[0];
+  for (i = 0; i < vlgFiles.size(); i++) {
+    printf("-I- Parsing verilog %s ...\n", vlgFiles[i].c_str());
+    if (!design->parseStructuralVerilog(vlgFiles[i].c_str())) {
+      cerr << "-E- Could not parse: " << vlgFiles[i] << " aborting." << endl;
+      exit(1);
+    }
+  }
+
+    hcmCell *topCell = design->getCell(topLevel);
+  if (!topCell) {
+    printf("-E- could not find cell %s\n", topLevel.c_str());
+    exit(1);
+  }
+  
+  vcdFormatter vcd(topLevel + ".vcd", topCell, globalNodes);
+  if (!vcd.good()) {
+    printf("-E- Could not create vcdFormatter for cell: %s\n", 
+           topLevel.c_str());
+    exit(1);
+  }
+ hcmCell *flatCell = hcmFlatten(topLevel + string("_flat"), topCell, globalNodes);
+ cout << "-I- Top cell flattened" << endl;
+ 
+ // Initialization of all states of all nodes in Topcell- we initialize all to 0 
+ 
+map<string, hcmNode* > topcell_nodes = flatCell->getNodes();  
+map<string, hcmNode* >::iterator nI;
+
+//We erase VDD and VSS from map of simulated nodes while assigning them to the desired value 
+ for(nI = topcell_nodes.begin();nI != topcell_nodes.end(); nI++){
+		hcmNode *node = (*nI).second;
+		node->setProp("cur_bool", false);
+		string nname = node->getName();
+		
+		if (nname == "VSS")
+		{
+			topcell_nodes.erase(nI);
+			continue;
+		}
+		if (nname == "VDD")
+		{
+			node->setProp("cur_bool", true); // VDD is '1'
+			topcell_nodes.erase(nI);
+			continue;
+		}
+			
+	}
+	
+vector<hcmInstance*> dff_instances; 
+map<string, hcmInstance*> all_instances = flatCell->getInstances();
+map<string, hcmInstance*>::iterator iI; 
+
+//create vector of dff's for preliminary simulation 
+for(iI = all_instances.begin();iI != all_instances.end();iI++){
+	string cur_inst = iI->second->masterCell()->getName();
+	if(cur_inst == "dff"){
+		dff_instances.push_back(iI->second);
+	}
+}
+
+int stime = 0;  // holds the simulation time 
+hcmNode* cur_n; //current node we operate on in the simulation 
+bool prev_val;
+bool cur_val; 
+queue<hcmNode*> events; 
+
+//We simulate the circuit as long as there are new signal values  
+while (parser.readVector() == 0) {
+	
+	//extract all signal values at current time 
 	 for (set<string>::iterator I= sigs.begin(); I != sigs.end(); I++) {
 		string name = (*I);
 		bool val;
 		parser.getSigValue(name, val);
-		cout << "  " << name << " = " << (val? "1" : "0")  << endl;
+		cur_n = topcell_nodes[name]; //extract pointer to the node signal represents 
+		cur_n->getProp("cur_bool",cur_val);
+		if(cur_val == val) continue; //case where the node value stays the same
+		else{ 
+		events.push(cur_n);
+		cur_n->setProp("cur_bool", val);
+		}
+			
 	 }
-	 cout << "-I- Reading next vectors ... " << endl;
+	 
+	 
+	 
+	stime++; 
   }
-  
-  set< string> globalNodes;
-  globalNodes.insert("VDD");
-  globalNodes.insert("VSS");
-
-  hcmDesign* design = new hcmDesign("design");
-  string cellName = vlgFiles[0];
-  for (i = 1; i < vlgFiles.size(); i++) {
-    printf("-I- Parsing verilog %s ...\n", vlgFiles[i].c_str());
-    if (!design->parseStructuralVerilog(vlgFiles[i].c_str())) {
-      cerr << "-E- Could not parse: " << vlgFiles[i] << " aborting." << endl;
-      exit(1);
-    }
-  }
-
-    hcmCell *topCell = design->getCell(cellName);
-  if (!topCell) {
-    printf("-E- could not find cell %s\n", cellName.c_str());
-    exit(1);
-  }
-  
-  vcdFormatter vcd(cellName + ".vcd", topCell, globalNodes);
-  if (!vcd.good()) {
-    printf("-E- Could not create vcdFormatter for cell: %s\n", 
-           cellName.c_str());
-    exit(1);
-  }
+	
 
 
 
+ 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  set< string> globalNodes;
-  globalNodes.insert("VDD");
-  globalNodes.insert("VSS");
-  
-  hcmDesign* design = new hcmDesign("design");
-  string cellName = vlgFiles[0];
-  for (i = 1; i < vlgFiles.size(); i++) {
-    printf("-I- Parsing verilog %s ...\n", vlgFiles[i].c_str());
-    if (!design->parseStructuralVerilog(vlgFiles[i].c_str())) {
-      cerr << "-E- Could not parse: " << vlgFiles[i] << " aborting." << endl;
-      exit(1);
-    }
-  }
-
-  hcmCell *topCell = design->getCell(cellName);
-  if (!topCell) {
-    printf("-E- could not find cell %s\n", cellName.c_str());
-    exit(1);
-  }
-  
-  vcdFormatter vcd(cellName + ".vcd", topCell, globalNodes);
-  if (!vcd.good()) {
-    printf("-E- Could not create vcdFormatter for cell: %s\n", 
-           cellName.c_str());
-    exit(1);
-  }
 }  
